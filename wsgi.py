9367f372591351ff842956f2d844719625429251
Michael Garcia <thecrazygm@gmail.com>
2025-06-27 04:43:15 -0400

    refactor: update WSGI configuration to use factory pattern with create_app

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,3 @@
-"""
-WSGI config for distriator.
+from app import create_app
 
-This module contains the WSGI application used by Gunicorn.
-"""
-
-from app import app
-
-# This makes the app available as 'application' for WSGI servers like Gunicorn
-application = app
-
-if __name__ == "__main__":
-    # This block is for development use only
-    # In production, use Gunicorn or another WSGI server
-    app.run(debug=False)
+app = create_app()
